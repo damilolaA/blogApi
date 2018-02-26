7823caa96b2bdc567c49ecff0f13a901f51f595a
dammy <adiodamilola@yahoo.com>
2018-02-26 14:53:02 +0100

    mount controllers on express router http methods

--- src/api/v1/posts/posts-router.js
+++ src/api/v1/posts/posts-router.js
@@ -1,1 +1,16 @@
- 
+const express = require('express'),
+	  router  = express.Router(),
+	  controllers = require('./posts-controllers.js');
+
+router.param('id', controllers.interceptPostId);
+
+router.route('/')
+	.post(controllers.addPost)
+	.get(controllers.getPosts)
+
+router.route('/:id')
+	.get(controllers.getPostById)
+	.delete(controllers.deletePost)
+	.put(controllers.updatePost)
+
+module.exports = router;
